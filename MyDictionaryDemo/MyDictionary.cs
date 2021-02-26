a900624443741c7816585c04f22ef0ebd402c553
GokhanCoban <gokhan.coban2516@gmail.com>
2021-02-26 15:23:25 +0300

    26.02.2021 - Keys ve Values Metolarında Değişiklik

--- MyDictionaryDemo/MyDictionary.cs
+++ MyDictionaryDemo/MyDictionary.cs
@@ -3,97 +3,107 @@ using System.Collections.Generic;
 
 namespace MyDictionaryDemo
 {
-    public class MyDictionary<TKey,TValue>
+    public class MyDictionary<TKey, TValue>
     {
-       private  TKey[] keys=null;
-       private TValue[] values=null;
-       private  int _capacity=0;
-       private const int _defaultCapacity=5;
-       private int _length=0;
-      
-      //Varsayılan olarak bu nesnenin kapasitesi 5 olacak şekilde başlar.
-      public MyDictionary()
-      {
-          _capacity=_defaultCapacity;
-          keys=new TKey[_capacity];
-          values=new TValue[_capacity];
+        private TKey[] keys = null;
+        private TValue[] values = null;
+        private int _capacity = 0;
+        private int _defaultCapacity = 5;
+        private int _length = 0;
 
-      }
-
-       //Parametre olarak kapasite alır.
-       public MyDictionary(int capacity)
+        //Varsayılan olarak bu nesnenin kapasitesi 5 olacak şekilde başlar.
+        public MyDictionary()
         {
-            _capacity=capacity;
-            keys=new TKey[_capacity];
-            values=new TValue[_capacity];
+            _capacity = _defaultCapacity;
+            keys = new TKey[_capacity];
+            values = new TValue[_capacity];
 
         }
-    
-       //Bu listeye anahtar ve değer biçiminde bir eleman ekler.
-       //Eğer kapasite eleman sayısına eşitse kapasiteyi 2 katına çıkarır.
-        public void Add(TKey key,TValue value)
+
+        //Parametre olarak kapasite alır.
+        public MyDictionary(int capacity)
+        {
+            _capacity = capacity;
+            keys = new TKey[_capacity];
+            values = new TValue[_capacity];
+
+        }
+
+        //Bu listeye anahtar ve değer biçiminde bir eleman ekler.
+        //Eğer kapasite eleman sayısına eşitse kapasiteyi 2 katına çıkarır.
+        public void Add(TKey key, TValue value)
         {
 
-             keys.SetValue(key,_length);
-             values.SetValue(value,_length);
-            _length+=1;
+            keys.SetValue(key, _length);
+            values.SetValue(value, _length);
+            _length += 1;
 
-            if (_capacity==_length)
+            if (_capacity == _length)
             {
-                int _newCapacity= _capacity*=2;
-               
-               TKey[] _tempKeys=keys;
-               TValue[] _tempValues=values;
+                int _newCapacity = _capacity *= 2;
 
-               keys=new TKey[_newCapacity];
-               values=new TValue[_newCapacity];
+                TKey[] _tempKeys = keys;
+                TValue[] _tempValues = values;
 
-               for (int i = 0; i < _length; i++)
-               {
-                   keys[i]=_tempKeys[i];
-                   values[i]=_tempValues[i];
-               }
-                
+                keys = new TKey[_newCapacity];
+                values = new TValue[_newCapacity];
+
+                for (int i = 0; i < _length; i++)
+                {
+                    keys[i] = _tempKeys[i];
+                    values[i] = _tempValues[i];
+                }
+
             }
         }
-       
-       //Bu listeye eklenen tüm "anahtarları" döndürür.
-        public TKey[] Keys {
-            
-             get {
-                 
-                  TKey[] allKeys=new TKey[_length];
-                 for (int i = 0; i <= _length-1; i++)
-                 {
-                     allKeys[i]=keys[i];
-                 }
-                 
-                 return allKeys;
-                 }
-        
-         }
 
-       //Bu listeye eklenen tüm "değerleri" döndürür.
-         public TValue[] Values 
-         {
-              get
-              {
-                  TValue[]  allValues=new TValue[_length];
+        //Bu listeye eklenen tüm "anahtarları" döndürür.
+        public TKey[] Keys
+        {
 
-                  for (int i = 0; i <_length; i++)
-                  {
-                      allValues[i]=values[i];
-                  }
-                  
-                  return allValues;
-              }
-         }
-       
-       //Bu listenin kapasitesini döndürür 
-        public  int  Capacity { get { return _capacity; }}
+            get
+            {
 
-       //Bu listenin eleman sayısını döndürür.
-        public int Length { get {return _length;} }
+                //Bu eski kod parçası alında bir altta sadece tek satır kod yetiyordu.
+
+                /* TKey[] allKeys=new TKey[_length];
+               for (int i = 0; i <= _length-1; i++)
+               {
+                   allKeys[i]=keys[i];
+               }
+
+               return allKeys; */
+
+                return keys;
+
+            }
+
+        }
+
+        //Bu listeye eklenen tüm "değerleri" döndürür.
+        public TValue[] Values
+        {
+            get
+            {
+                //Bu eski kod parçası alında bir altta sadece tek satır kod yetiyordu.
+                /* TValue[]  allValues=new TValue[_length];
+
+                for (int i = 0; i <_length; i++)
+                {
+                    allValues[i]=values[i];
+                }
+
+                return allValues; */
+
+                return values;
+            }
+        }
+
+        //Bu listenin kapasitesini döndürür 
+        public int Capacity { get { return _capacity; } }
+
+        //Bu listenin eleman sayısını döndürür.
+        public int Length { get { return _length; } }
 
 
     }
--- MyDictionaryDemo/Program.cs
+++ MyDictionaryDemo/Program.cs
@@ -7,47 +7,65 @@ namespace MyDictionaryDemo
         static void Main(string[] args)
         {
 
-            MyDictionary<string,string> myDictionary=new MyDictionary<string, string>(7);
-            myDictionary.Add("76232321626","Engin Demiroğ");
-            myDictionary.Add("12385469857","Gökhan Çoban");
-            myDictionary.Add("43598651421","Ercan Uçar");
-            myDictionary.Add("78933223655","Akın Aras");
-           
-    
-            
-          
-            
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>(7);
+            myDictionary.Add("76232321626", "Engin Demiroğ");
+            myDictionary.Add("12385469857", "Gökhan Çoban");
+            myDictionary.Add("43598651421", "Ercan Uçar");
+            myDictionary.Add("78933223655", "Akın Aras");
 
-            int cap= myDictionary.Capacity;
-            int len=myDictionary.Length;
+
+
+
+
+
+            int cap = myDictionary.Capacity;
+            int len = myDictionary.Length;
             Console.WriteLine("Kapasite : " + cap);
             Console.WriteLine("Eleman Sayısı : " + len);
-           
 
-            string[] keys= myDictionary.Keys;
-            string[] values=myDictionary.Values;
+
+            string[] keys = myDictionary.Keys;
+            string[] values = myDictionary.Values;
 
             Console.WriteLine("-------Keys-------");
             foreach (var item in keys)
-            {  
-                if (item!=null)
-                {    
-                    
-                     Console.WriteLine("Key : " + item);
+            {
+                if (item != null)
+                {
+
+                    Console.WriteLine("Key : " + item);
                 }
-                
+
             }
-             
+
             Console.WriteLine("-------Values-------");
             foreach (var item in values)
             {
-                if (item!=null)
+                if (item != null)
                 {
-                    
+
                     Console.WriteLine("Value : " + item);
                 }
-                
+
             }
+
+            //Çıktı Şöyle olacaktır.
+            /* 
+
+            Kapasite: 7
+            Eleman Sayısı : 4
+            --------Keys--------
+            Key: 76232321626
+            Key: 12385469857
+            Key: 43598651421
+            Key: 78933223655
+            --------Values--------
+            Value: Engin Demiroğ
+            Value: Gökhan Çoban
+            Value: Ercan Uçar
+            Value: Akın Aras 
+
+            */
 
         }
     }
